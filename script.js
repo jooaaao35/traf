c0024a39ebe3741d08a174708037f59d5e64b861
jooaaao35 <joaof0666@gmail.com>
2025-07-01 01:30:55 -0300

    1

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function irParaExemplo(exemplos) {
-    window.location.href = exemplos;
+function mostrarAula() {
+    let conteudo = document.getElementById('conteudo');
+    conteudo.innerHTML = 'Conteúdo da pasta: Aula 2 - Jogo Numero Secreto';
+}
+
+function mostrarExemplo1() {
+    let conteudo = document.getElementById('conteudo');
+    conteudo.innerHTML = 'Conteúdo da pasta: Exemplo 1 - Formulário com salvamento em txt';
+}
+
+function mostrarExemplo2() {
+    let conteudo = document.getElementById('conteudo');
+    conteudo.innerHTML = 'Conteúdo da pasta: Exemplo 2 - Lista';
 }
