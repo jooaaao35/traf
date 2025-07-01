e41670d3ca87a0f0b21b605dbef6f00f8097fbde
jooaaao35 <joaof0666@gmail.com>
2025-07-01 01:58:35 -0300

    3

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-function mostrarAula() {
-    let conteudo = document.getElementById('conteudo');
-    conteudo.innerHTML = 'Aula 2 - JogoNumeroSecretoMaisInterativo-20250628';
+function irParaJogoNumeroSecreto() {
+    window.location.href = 'Aula 2 - JogoNumeroSecretoMaisInterativo-20250628/jogo.html';
 }
 
-function mostrarExemplo1() {
-    let conteudo = document.getElementById('conteudo');
-    conteudo.innerHTML = 'Exemplo 1 - Formulário com salvamento em txt-20250628';
+function irParaFormularioComSalvamento() {
+    window.location.href = 'Exemplo 1 - Formulário com salvamento em txt-20250628/formulario.html';
 }
 
-function mostrarExemplo2() {
-    let conteudo = document.getElementById('conteudo');
-    conteudo.innerHTML = 'Exemplo 2 - Lista-20250628';
+function irParaLista() {
+    window.location.href = 'Exemplo 2 - Lista-20250628/lista.html';
 }
