756f8443766ae111b9f62be79011b82e66cf7224
jooaaao35 <joaof0666@gmail.com>
2025-07-01 02:19:04 -0300

    1

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function irParaJogoNumeroSecreto() {
-    window.location.href = 'Aula 2 - JogoNumeroSecretoMaisInterativo-20250628/jogo.html';
+function irParaExemploFormulario() {
+    window.location.href = 'Exemplo 1 - Formulário com salvamento em txt-20250628/index.html';
 }
 
-function irParaFormularioComSalvamento() {
-    window.location.href = 'Exemplo 1 - Formulario com salvamento em txt-20250628/formulario.html';
+function irParaExemploLista() {
+    window.location.href = 'Exemplo 2 - Lista-20250628/index.html';
 }
 
-function irParaLista() {
-    window.location.href = 'Exemplo 2 - Lista-20250628/lista.html';
+function irParaExemploJogo() {
+    window.location.href = 'Aula 2 - JogoNumeroSecretoMaisInterativo-20250628/index.html';
 }
